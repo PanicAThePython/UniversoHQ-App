eca4f744f15a515852415d4943db52f1ec7f551e
PanicAThePython <nataliaweise@gmail.com>
2021-05-31 10:29:10 -0300

    Filter data function complete

--- v2/page-scripts/table-filter.js
+++ v2/page-scripts/table-filter.js
@@ -2,28 +2,31 @@ var searchCharacter = document.querySelector('#filter_table')
 
 searchCharacter.addEventListener('input', function(){
     var characters = document.querySelectorAll('.character')
+    var divisions = document.querySelectorAll(".division")
 
     if (this.value.length > 0){
         for (var i = 0; i < characters.length; i++){
             var character = characters[i]
+            var division = divisions[i+1]
             console.log(character)
             var characterContent = character.textContent
             var expression = new RegExp(this.value, "i")
             if(!expression.test(characterContent)){
                 character.classList.add("invisible")
-                character.classList.remove("division")
+                division.classList.add("invisible")
             }
             else{
                 character.classList.remove("invisible")
-                //character.classList.add("division")
+                division.classList.remove("invisible")
             }
         }
     }
     else{
         for(var i = 0; i < characters.length; i++){
             var character = characters[i]
+            var division = divisions[i+1]
             character.classList.remove("invisible")
-            //character.classList.add("division")
+            division.classList.remove("invisible")
         }
     }
 })
