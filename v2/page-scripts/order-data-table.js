6e5871aa0380fa9291daf9c21a9281ccf2322902
PanicAThePython <nataliaweise@gmail.com>
2021-06-02 14:45:32 -0300

    Order by name and ego almost complete

--- v2/page-scripts/order-data-table.js
+++ v2/page-scripts/order-data-table.js
@@ -1,18 +1,20 @@
 function orderBy(){
     let selectValue = document.querySelector("#order_by").value
-
     switch(selectValue){
         case "0":
-        
+            let ordedByEgo = orderByEgo()
+            clearTable()
+            replaceDataTable(ordedByEgo)
             break
         case "1":
-            
+            let ordedByEgoReverse = orderByEgo().reverse()
+            clearTable()
+            replaceDataTable(ordedByEgoReverse)
             break
         case "2":
             let ordedByName = orderByName()
             clearTable()
             replaceDataTable(ordedByName)
-            
             break
         case "3":
             let ordedByNameReverse = orderByName().reverse()
@@ -26,7 +28,6 @@ function orderBy(){
 
 function orderByName(){
     let characters = document.querySelectorAll(".character")
-    let charactersName = document.querySelectorAll(".character-name")
     let counter = 0
     let ordedCharacters = []
 
@@ -50,29 +51,55 @@ function orderByName(){
     return ordedCharacters;
 }
 
-function clearTable(){
-    let characters = document.querySelector("#listagem-body").rows
-    let divisions = document.querySelectorAll(".division")
-    
+function orderByEgo(){
+    let characters = document.querySelectorAll(".character")
+    let counter = 0
+    let ordedCharacters = []
+
     for (var i = 0; i < characters.length; i++){
-        divisions[i+1].remove()
-        document.querySelector("#listagem-body").deleteRow(i)
+        if (ordedCharacters.length == 0){
+            ordedCharacters[counter] = characters[i]
+        }
+        else{
+            if (characters[i].innerText.split("\t")[1] < ordedCharacters[counter].innerText.split("\t")[1]){
+                ordedCharacters[counter+1] = ordedCharacters[counter]
+                ordedCharacters[counter] = characters[i]
+                counter++
+            }
+            else{
+                ordedCharacters[counter+1] = characters[i]
+                counter++
+            }  
+        }
+    }
+
+    return ordedCharacters;
+}
+
+function clearTable(){
+    let rows = document.querySelectorAll(".row")
+    
+    for (var i = 0; i < rows.length; i++){
+        rows[i].remove()
     }
 }
 
 function replaceDataTable(ordedByName){
     let characters = document.querySelector("#listagem-body")
-    let tr = document.createElement('tr')
-    let td = document.createElement('td')
-    let div = document.createElement('div')
-
-    div.classList.add('division')
-    td.setAttribute("colspan", "6")
-    td.append(div)
-    tr.append(td)
 
     for (var i = 0; i < ordedByName.length; i++){
         characters.append(ordedByName[i])
+
+        let tr = document.createElement('tr')
+        let td = document.createElement('td')
+        let div = document.createElement('div')
+
+        div.classList.add('division')
+        td.setAttribute("colspan", "6")
+        td.append(div)
+        tr.append(td)
+        tr.classList.add('row')
+
         characters.appendChild(tr)
     }
 }
